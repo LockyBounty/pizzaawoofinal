3a0bc6ace20470f3482772b88bc41c5bb1eff494
lockybounty <bounthienglocky@gmail.com>
2021-02-14 04:35:42 +0100

    added - footer page

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,26 @@
+const Footer = () => (
+  <div>
+    <div>
+      <ul>
+        <h3>À propos de Pizza&Awoo</h3>
+        <li>La marque</li>
+        <li>emploi</li>
+        <li>Franchise</li>
+        <li>Contact</li>
+      </ul>
+      <ul>
+        <h3>Commandez en ligne</h3>
+        <li>Livraison</li>
+        <li>Take out</li>
+      </ul>
+      <ul>
+        <h3>Nos restaurants</h3>
+        <li>Où nous trouver</li>
+        <li>La carte de menu</li>
+        <li>Student Deals</li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Footer;
