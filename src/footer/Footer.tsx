50590ca73a2df4776d6f98f8669e82adeb82a41d
lockybounty <bounthienglocky@gmail.com>
2021-02-18 01:50:33 +0100

    update - footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -3,24 +3,30 @@ import './footer.scss';
 const Footer = () => (
   <div className="footer-main">
     <div className="footer-first">
-      <ul>
-        <h3>À propos de Pizza Awoo</h3>
-        <li>La marque</li>
-        <li>emploi</li>
-        <li>Franchise</li>
-        <li>Contact</li>
-      </ul>
-      <ul>
-        <h3>Commandez en ligne</h3>
-        <li>Livraison</li>
-        <li>Take out</li>
-      </ul>
-      <ul>
-        <h3>Nos restaurants</h3>
-        <li>Où nous trouver</li>
-        <li>La carte de menu</li>
-        <li>Student Deals</li>
-      </ul>
+      <div>
+        <ul>
+          <h3>À propos de Pizza Awoo</h3>
+          <li>La marque</li>
+          <li>emploi</li>
+          <li>Franchise</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+          <ul>
+            <h3>Commandez en ligne</h3>
+            <li>Livraison</li>
+            <li>Take out</li>
+          </ul>
+      </div>
+      <div>
+          <ul>
+            <h3>Nos restaurants</h3>
+            <li>Où nous trouver</li>
+            <li>La carte de menu</li>
+            <li>Student Deals</li>
+          </ul>
+      </div>
     </div>
     <div className="footer-second">
       <div className="footer-social">
