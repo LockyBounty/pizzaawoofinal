f34d041f87dd5d3598c590d72a47f183e4096117
lockybounty <bounthienglocky@gmail.com>
2021-02-23 03:37:42 +0100

    added - lines Subscription

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,50 +1,60 @@
 import './footer.scss';
+import React, {useState} from "react";
 
-const Footer = () => (
-  <div className="footer-main">
-    <div className="footer-first">
-      <div>
-        <ul>
-          <h3>À propos de Pizza Awoo</h3>
-          <li>La marque</li>
-          <li>emploi</li>
-          <li>Franchise</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div>
+function Footer(){
+
+  
+  return (
+    <div className="footer-main">
+      <div className="footer-first">
+        <div>
           <ul>
-            <h3>Commandez en ligne</h3>
-            <li>Livraison</li>
-            <li>Take out</li>
+            <h3>À propos de Pizza Awoo</h3>
+            <li>La marque</li>
+            <li>emploi</li>
+            <li>Franchise</li>
+            <li>Contact</li>
           </ul>
-      </div>
-      <div>
-          <ul>
-            <h3>Nos restaurants</h3>
-            <li>Où nous trouver</li>
-            <li>La carte de menu</li>
-            <li>Student Deals</li>
-          </ul>
-      </div>
-    </div>
-    <div className="footer-second">
-      <div className="footer-social">
-        <ul>
-          <li>fb</li>
-          <li>yt</li>
-          <li>ig</li>
-        </ul>
-      </div>
-      <div className="footer-signature">
-        <h5>@2021 Pizza Awoo</h5>
-      </div>
-      <div className="footer-rules">
-        <a href="/">Conditions générales </a>
-        <a href="/">Cookies</a>
         </div>
+        <div>
+            <ul>
+              <h3>Commandez en ligne</h3>
+              <li>Livraison</li>
+              <li>Take out</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+              <h3>Nos restaurants</h3>
+              <li>Où nous trouver</li>
+              <li>La carte de menu</li>
+              <li>Student Deals</li>
+            </ul>
+        </div>
+      </div>
+      <div className="footer-second">
+        <div className="footer-social">
+          <ul>
+            <li>fb</li>
+            <li>yt</li>
+            <li>ig</li>
+          </ul>
+        </div>
+        <div className="footer-signature">
+          <h5>@2021 Pizza Awoo</h5>
+        </div>
+        <div className="footer-rules">
+          <a href="/">Conditions générales </a>
+          <a href="/">Cookies</a>
+        </div>
+        
+      </div>
     </div>
-  </div>
-);
+  );
+
+  
+
+
+} 
 
 export default Footer;
--- src/pages/Subscription.tsx
+++ src/pages/Subscription.tsx
@@ -1,41 +1,65 @@
-const Subscription = () => (
-
-<div>
-    <div><a href="/">Retour</a></div>
-    <h2>INSCRIPTION</h2>
-    Déjà un compte ? <a href="./Login">Se connecter</a>
-    <form onSubmit={handleSubmit} method="post" name="login1">
-        <Email />
-        <Pass />
-        <ConfirmPass />
-        <button type="submit">Valider</button>
-    </form>
-    
-</div>
-);
-
-const Email = () => (
-    <div>
-        <input type="email" name="email" id="email2"/>
-    </div>
-);
-
-const Pass = () => (
-    <div>
-        <input type="password" name="pass" id="pass2"/>
-    </div>
-);
-
-const ConfirmPass = () => (
-    <div>
-        <input type="password" name="confirmPass" id="confirmPass2"/>
-    </div>
-);
+import React, { useState, useReducer } from "react";
 
 function handleSubmit(e) {
-    e.preventDefault();
-    console.log(e.target[0].value)
+  e.preventDefault();
+  console.log(e.target[0].value);
 }
 
+function Subscription() {
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const onChangeMail = (event) => {
+    setMail(event.target.value);
+  };
+  const onChangePass = (event) => {
+    setPass(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <a href="/">Retour</a>
+      </div>
+      <h2>INSCRIPTION</h2>
+      Déjà un compte ? <a href="./Login">Se connecter</a>
+      <form onSubmit={handleSubmit} method="post" name="login1">
+        <div className="email-block">
+          <input
+            type="email"
+            name="email"
+            id="email2"
+            placeholder="Email"
+            onChange={onChangeMail}
+            value={mail}
+            required
+          />
+        </div>
+        <div className="pass-block">
+          <input
+            type="password"
+            name="pass"
+            id="pass2"
+            placeholder="Mot de passe"
+            onChange={onChangePass}
+            value={pass}
+            min={6}
+            required
+          />
+        </div>
+        <div className="pass-check-block">
+          <input
+            type="password"
+            name="confirmPass"
+            id="confirmPass2"
+            placeholder="Confirmez le mot de passe"
+            required
+          />
+        </div>
+        <button type="submit">Valider</button>
+      </form>
+    </div>
+  );
+}
 
 export default Subscription;
