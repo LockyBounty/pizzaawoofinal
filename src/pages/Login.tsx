2d5e07dc3c5a0a5337450efb1d926e760e384db3
lockybounty <bounthienglocky@gmail.com>
2021-03-08 02:40:04 +0100

    added - form lines

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,40 +1,93 @@
-const Login = () => (   
+import { useState, useReducer } from 'react';
 
-<div className="loginMain">
-    <div className="return">
-        <a href="/">Retour</a>
-    </div>
-    <div className="loginInside">
-        <h2>CONNEXION</h2>
-        Pas de compte ? <a href="./Subscription">S'inscrire</a>
-        <form onSubmit={handleSubmit} method="post" name="login1">
-            <Email />
-            <Pass />
-            <button type="submit">Valider</button>
-        </form>
-    </div>
+
+function Login(){
+
+    const [mail, setMail] = useState("");
+    const [pass, setPass] = useState("");
+
+    const onChangeMail = (event) => {
+        setMail(event.target.value);
+      };
+    const onChangePass = (event) => {
+    setPass(event.target.value);
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!e.target.checkValidity()) {
+            // form is invalid! so we do nothing
+            return;
+          }
+        
+        const data = new FormData(e.target);
+         
+        data.set('email', mail);
+        data.set('pass', pass);
+
+
+        console.log(data.get("email"))
+
+        
+    }
     
-</div>
 
-);
 
-const Email = () => (
-    <div>
-        <input type="email" name="email" id="email1"/>
-    </div>
-);
-
-const Pass = () => (
-    <div>
-        <input type="password" name="pass" id="pass"/>
-    </div>
-);
-
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log(e.target[0].value)
+    return (
+        <div className="loginMain">
+            <div className="return">
+                <a href="/">Retour</a>
+            </div>
+            <div className="loginInside">
+                <h2>CONNEXION</h2>
+                Pas de compte ? <a href="./Subscription">S'inscrire</a>
+                <form onSubmit={handleSubmit} method="post" name="login1">
+                    
+                        <input type="email" 
+                        name="email" 
+                        id="email1"
+                        placeholder="Email"
+                        onChange={onChangeMail}
+                        value={mail}
+                        />
+                    
+                    
+                    
+                        <input type="password" 
+                        name="pass" 
+                        id="pass1"
+                        placeholder="Mot de passe"
+                        onChange={onChangePass}
+                        value={pass}
+                        />
+                    
+                    <button type="submit">Valider</button>
+                </form>
+            </div>
+            <div>
+            </div>
+            
+        </div>
+    );
 }
 
+
+
+//Exemple envoi vers back 
+// function handleSubmit(e) {
+//     e.preDefault();
+//     const data = new FormData(e.target);
+//     // NOTE: you access FormData fields with `data.get(fieldName)`    
+//     const [month, day, year] = data.get('birthdate').split('/');
+//     const serverDate = `${year}-${month}-${day}`;
+//     data.set('birthdate', serverDate);
+//     data.set('username', data.get('username').toUpperCase());
+//     fetch('/api/form-submit-url', {
+//       method: 'POST',
+//       body: data,
+//     });
+// }
 
 
 export default Login;
