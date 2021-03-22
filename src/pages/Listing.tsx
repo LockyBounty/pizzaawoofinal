bb6eb462cb97b9995c7b5936300a1f5b6f76ca6b
lockybounty <bounthienglocky@gmail.com>
2021-03-22 07:55:23 +0100

    Added - img

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -6,6 +6,11 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
+import CreateIcon from '@material-ui/icons/Create';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+// import IconButton from '@material-ui/icons/IconButton';
+
 import { Pizzas } from '../utils/Recipe'
 import "../styles/listing.scss"
 
@@ -19,6 +24,11 @@ const Listing = () =>
                 <Card 
                 className="pizza_list_card"
                 key={index}>
+                    <CardMedia
+                    // className={classes.media}
+                    // image="/static/images/cards/contemplative-reptile.jpg"
+                    // title="Contemplative Reptile"
+                    />
                     <CardContent>
 
                     
@@ -38,7 +48,24 @@ const Listing = () =>
                             </ul>
                         </p>
                         <p>{pizza.prix} €</p>
+
                     </CardContent>
+                    <div className="card_button_zone">
+                        <Button 
+                        className="card_button" 
+                        variant="contained" 
+                        color="secondary">
+                            <RestaurantIcon  className="button_icon"/> 
+                            J'veux ça !
+                        </Button>
+                        <Button 
+                        className="card_button" 
+                        variant="contained" 
+                        color="secondary">
+                            <CreateIcon  className="button_icon"/> 
+                            modifier
+                        </Button>
+                    </div>
                 </Card>
             ))
         }
