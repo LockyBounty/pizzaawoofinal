d9ef2a970f74e02bcd44d5bc470f569e174d46e4
lockybounty <bounthienglocky@gmail.com>
2021-04-02 02:03:42 +0200

    Added - card content fixes

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -3,13 +3,13 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
-import CreateIcon from '@material-ui/icons/Create';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import CreateIcon from '@material-ui/icons/CreateOutlined';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingBasketOutlined';
 import RestaurantIcon from '@material-ui/icons/Restaurant';
-// import IconButton from '@material-ui/icons/IconButton';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 import { Pizzas } from '../utils/Recipe'
 import "../styles/listing.scss"
@@ -49,25 +49,30 @@ const Listing = () =>
                                 )}
                             </ul>
                         </p>
-                        <p>{pizza.prix} €</p>
 
                     </CardContent>
                     
                     <div className="card_button_zone">
-                        <Button 
-                        className="card_button" 
-                        variant="contained" 
-                        color="secondary">
-                            <RestaurantIcon  className="button_icon"/> 
-                            J'veux ça !
-                        </Button>
-                        <Button 
-                        className="card_button" 
-                        variant="contained" 
-                        color="secondary">
-                            <CreateIcon  className="button_icon"/> 
-                            modifier
-                        </Button>
+                        <Chip 
+                        className="pizza_price_chip"
+                        color="secondary"
+                        label={`${pizza.prix} €`} 
+                        />
+                        <div>
+                            <IconButton 
+                            color="secondary">
+                                <ShoppingCartIcon /> 
+                            </IconButton>
+                            
+                            <IconButton 
+                            aria-label="modify"
+                            color="primary">
+                                <CreateIcon 
+                                color="secondary"
+                                />
+                            </IconButton>
+
+                        </div>
                     </div>
                 </Card>
             ))
